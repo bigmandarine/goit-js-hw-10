ce9e9c8aa392853bbd5c1df0d93958a48e48fb43
Dima Kuzko <bigmandarine1991@gmail.com>
2022-09-08 22:38:08 +0300

    begin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+function inputSearchCountry() {
+  console.log(fetchCountries(refs.input.value));
+}
+refs.input.addEventListener(
+  'input',
+  debounce(() => {
+    inputSearchCountry();
+  }, DEBOUNCE_DELAY)
+);
