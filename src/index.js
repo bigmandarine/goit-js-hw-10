49252da874042c50811e5d1735de7a849a342094
Dima Kuzko <bigmandarine1991@gmail.com>
2022-09-11 19:34:15 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,13 +1,74 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  oneCountryInfo: document.querySelector('.country-info'),
 };
 function inputSearchCountry() {
-  console.log(fetchCountries(refs.input.value));
+  fetchCountries(refs.input.value)
+    .then(countries => {
+      if (refs.input.value !== '') {
+        if (countries.length > 10) {
+          return Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (countries.length >= 2 && countries.length <= 10) {
+          refs.countryList.innerHTML = '';
+          refs.oneCountryInfo.innerHTML = '';
+          renderAllContries(countries);
+          // console.log(fetchCountries(refs.input.value));
+        } else if (countries.length === 1) {
+          refs.countryList.innerHTML = '';
+          refs.oneCountryInfo.innerHTML = '';
+          renderOneCountry(countries);
+        }
+        console.log(fetchCountries(refs.input.value));
+      } else if (!countries.name.contains(refs.input.value)) {
+        return Notiflix.Notify.failure(
+          'Oops, there is no country with that name'
+        );
+      }
+    })
+    .catch(() =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
 }
+
+function renderAllContries(countries) {
+  const allContries = countries
+    .map(
+      country =>
+        `<li><img alt="Flag" src="${country.flags.svg}" width="30" height="20"/> <b>${country.name.common}</b></li>`
+    )
+    .join('');
+  console.log(allContries);
+  return refs.countryList.insertAdjacentHTML('beforeend', allContries);
+}
+
+function renderOneCountry(countries) {
+  const oneCountry = countries
+    .map(
+      country =>
+        `<h1><img alt="Flag" src="${
+          country.flags.svg
+        }" width="40" height="25"/> ${
+          country.name.common
+        }</h1><ul><li><b>Capital:</b> ${
+          country.capital
+        }</li><li><b>Population:</b> ${
+          country.population
+        }</li><li><b>Languages:</b> ${Object.values(
+          country.languages
+        )}</li></ul>`
+    )
+    .join('');
+  return refs.oneCountryInfo.insertAdjacentHTML('beforeend', oneCountry);
+}
+
 refs.input.addEventListener(
   'input',
   debounce(() => {
